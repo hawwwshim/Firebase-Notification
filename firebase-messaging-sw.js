ec8cb35ef44731da111fe4b3f6e3ea90e4826ecf
hawwwshim <166034398+hawwwshim@users.noreply.github.com>
2024-07-25 17:51:33 +0400

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+// importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
+
+// const firebase = {
+//   apiKey: "AIzaSyAlzNH8tozWok-yl-6Fc-t07ylnzq8JPds",
+//   authDomain: "kynotifications.firebaseapp.com",
+//   projectId: "kynotifications",
+//   storageBucket: "kynotifications.appspot.com",
+//   messagingSenderId: "251348178033",
+//   appId: "1:251348178033:web:39401dcfcefc81645777d0",
+//   measurementId: "G-VXQ7SZV0KF",
+// };
+
+// const messaging = firebase.messaging();
+
+// messaging.onBackgroundMessage(function (payload) {
+//   console.log("Received background message ", payload);
+//   const notificationTitle = "Background Message Title";
+//   const notificationOptions = {
+//     body: "Background Message body.",
+//     icon: "/firebase-logo.png",
+//   };
+
+//   return self.registration.showNotification(
+//     notificationTitle,
+//     notificationOptions
+//   );
+// });
